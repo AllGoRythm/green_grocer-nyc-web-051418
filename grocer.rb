c41fc1784098629193ecc4899a83a8ba0ef93472
Kurt Bauer <kurtbauer55@protonmail.com>
2018-05-09 00:23:17 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -9,7 +9,7 @@ def consolidate_cart(cart)
       description_arr.each_with_index do |food, index|
         items[food] = {}
         items[food] = description_arr[1]
-        items[food][description_arr][:count]
+        items[food][description_arr[1]][:count]
         binding.pry
       end
     end
