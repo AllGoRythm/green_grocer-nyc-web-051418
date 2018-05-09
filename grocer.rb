bb1245bd10c16890eb0107aeffd00835dd74a410
Kurt Bauer <kurtbauer55@protonmail.com>
2018-05-09 00:04:44 +0000

    automatically backed up by learn

--- grocer.rb
+++ grocer.rb
@@ -4,7 +4,7 @@ def consolidate_cart(cart)
 
   items = {}
 
-  cart.each do |k, v|
+  cart.each do |description_hash|
     description_hash.each do |description_arr|
       description_arr.each do |food, level_three|
         items[food] = {}
